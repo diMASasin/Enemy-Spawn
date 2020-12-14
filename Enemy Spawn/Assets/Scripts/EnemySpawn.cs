a794f711dcfe70cbd8e14cbce423e04b24252345
diMASasin <35368058+diMASasin@users.noreply.github.com>
2020-12-14 21:51:58 +0500

    Update EnemySpawn.cs

--- Enemy Spawn/Assets/Scripts/EnemySpawn.cs
+++ Enemy Spawn/Assets/Scripts/EnemySpawn.cs
@@ -6,15 +6,16 @@ public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private GameObject[] _spawnPoints = new GameObject[5];
     [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _timeBetweenSpawns;
 
     public IEnumerator SpawnEnemy()
     {
-        var waitForTwoSeconds = new WaitForSeconds(2f);
+        var waitForSeconds = new WaitForSeconds(_timeBetweenSpawns);
 
         while(true)
         {
             GameObject newEnemy = Instantiate(_enemy, _spawnPoints[Random.Range(0, _spawnPoints.Length)].transform);
-            yield return waitForTwoSeconds;
+            yield return waitForSeconds;
             Destroy(newEnemy);
         }
     }
